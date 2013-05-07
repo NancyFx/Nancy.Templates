d8cb55d00a96cabd059553899ed81ad982b54324
Christiaan baes <chris.baes@gmail.com>
2013-05-07 21:56:40 +0200

    All VB templates made.

---
+++ src/Nancy.Templates/Nancy.Templates.VisualBasic.AspNetHost/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Nancy.Templates.VisualBasic.AspNetHost")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Nancy.Templates.VisualBasic.AspNetHost")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ce5b1245-9a65-44c9-a368-05caa1bbbaf9")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ src/Nancy.Templates/Nancy.Templates.VisualBasic.SelfHostWithRazor/Bootstrapper.vb
@@ -0,0 +1,6 @@
+﻿Public Class Bootstrapper
+    Inherits DefaultNancyBootstrapper
+    ' The bootstrapper enables you to reconfigure the composition of the framework,
+    ' by overriding the various methods and properties.
+    ' For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+End Class

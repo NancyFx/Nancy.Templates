39adf6d37af6b584ad29dcc89a866a1cf8755fb1
Christiaan baes <chris.baes@gmail.com>
2013-05-07 22:37:29 +0200

    Adapted Assemblyinfos to match the CSharp ones.

--- src/Nancy.Templates/Nancy.Templates.VisualBasic.AspNetHost/My Project/AssemblyInfo.vb
+++ src/Nancy.Templates/Nancy.Templates.VisualBasic.AspNetHost/My Project/AssemblyInfo.vb
@@ -7,10 +7,10 @@ Imports System.Runtime.InteropServices
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
-<Assembly: AssemblyTitle("Nancy.Templates.VisualBasic.AspNetHost")> 
+<Assembly: AssemblyTitle("$safeprojectname$")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("Nancy.Templates.VisualBasic.AspNetHost")> 
+<Assembly: AssemblyProduct("$safeprojectname$")> 
 <Assembly: AssemblyCopyright("Copyright ©  2013")> 
 <Assembly: AssemblyTrademark("")> 
 

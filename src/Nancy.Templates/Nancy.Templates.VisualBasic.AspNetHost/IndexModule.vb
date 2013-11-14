2657c6944dbf9bc02f6b2c598789dedeb54fc965
Mattias Karlsson <matkar@wcom.se>
2013-11-14 16:20:16 +0100

    Upgraded VSIX to support Visual Studio 2013 1.Upgraded solution to VS2013 and VS2013SDK 2.Added VS version 12 as target 3.Upgraded NuGet dependency to 67e54e40-0ae3-42c5-a949-fddf5739e6a5 4.Upgraded Nancy to 0.21.1 5.Upgraded C# project files hint path to use solution dir 6.Upgraded C# template zip files 7.By request removed VB templates

--- src/Nancy.Templates/Nancy.Templates.VisualBasic.AspNetHost/IndexModule.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class IndexModule
-    Inherits NancyModule
-
-    Public Sub New()
-        MyBase.Get("/") = Function(parameters)
-                              Return View("index")
-                          End Function
-    End Sub
-End Class

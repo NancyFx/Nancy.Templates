fb3837dc077d059de953a891bed8b3e958506f56
Mattias Karlsson <matkar@wcom.se>
2013-11-28 08:27:29 +0100

    Cleanup 1.Cleaned up Nancy.Templates.csproj 2.Fixed formatting in web.config files 3.Adjusted project dependencies for correct build order 4.Added missing AssemblyInfo.cs in CSharp.EmptySelfHost csproj 5.Updated Template files

--- src/Nancy.Templates/Nancy.Templates.CSharp.EmptySelfHost/Properties/AssemblyInfo.cs
+++ src/Nancy.Templates/Nancy.Templates.CSharp.EmptySelfHost/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b6305ee-08fb-45ee-aa8c-65f8699365d0")]
+[assembly: Guid("6a86462d-b2a5-4995-8092-47b52745611b")]
 
 // Version information for an assembly consists of the following four values:
 //
